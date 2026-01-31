6cf0ac6403dd68c9cf296696144e45f27303c9f8
Mike <madamovych@gmail.com>
2026-01-31 02:40:11 +0200

    Moved code around. Optional, AssertState to be used with err.Recover when needed

---
+++ util/err/err.go
@@ -0,0 +1,29 @@
+package err
+
+import (
+	"fmt"
+	"os"
+	"runtime/debug"
+)
+
+/*
+	Recover must be used with defer.
+
+Prints unhandled error stack trace into stderr by default.
+*/
+func Recover(handler ...func(any)) {
+	if err := recover(); err != nil {
+		if len(handler) == 0 {
+			fmt.Fprintf(os.Stderr, "Unhandled error: %T: %v\n%s", err, err, debug.Stack())
+			return
+		}
+		for _, handler := range handler {
+			doHandle(err, handler)
+		}
+	}
+}
+
+func doHandle(err any, handler func(any)) {
+	defer Recover()
+	handler(err)
+}
